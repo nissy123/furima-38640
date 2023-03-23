fb25784b9da282a56594a42e392a002333acae88
nissy123 <nishinishioka66@gmail.com>
2023-03-23 11:57:44 +0900

    画像バリデーション実装

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ApplicationRecord
   has_one_attached :image
+
+  validates :product, presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.image.attached?
+  end
 end
