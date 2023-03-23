a4d9465d6b0598e72e347d44d0a0db8277a9cd19
nissy123 <nishinishioka66@gmail.com>
2023-03-23 11:50:34 +0900

    商品出品画面遷移・Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,18 @@ class ItemsController < ApplicationController
   def index
     
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    Product.create(product_params)
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:'#', :'#', :image).merge('#': current_user.id)
+  end
+  
 end
