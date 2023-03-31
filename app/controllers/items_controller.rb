72cb07d87336261e5dfeccd9224286b1235dba3e
nissy123 <nishinishioka66@gmail.com>
2023-03-31 21:08:51 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,6 @@ class ItemsController < ApplicationController
   before_action :authenticate_user! ,except: [:index]
   
   def index
-    @products = Product.all
     @products = Product.order("created_at DESC")
   end
 
