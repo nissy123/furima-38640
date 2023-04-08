9979ef6201ee3f4340356bc15efde5bb720d0e4e
nissy123 <nishinishioka66@gmail.com>
2023-04-08 21:09:57 +0900

    修正2

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,5 +1,5 @@
 class PurchaseRecordsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!
   before_action :set_purchase_record, only: [:index, :create]
 
   def index
